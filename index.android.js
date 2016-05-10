228f9b095949220df4af42b742f20c4c2570bb63
Jickson P <jickson.p@peppertap.com>
2016-05-10 22:46:44 +0530

    formatted index.android.js according to lint suggestions

--- index.android.js
+++ index.android.js
@@ -8,12 +8,31 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
 
 class ReactTodo extends Component {
   render() {
-    return (
+      return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
@@ -28,24 +47,5 @@ class ReactTodo extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('ReactTodo', () => ReactTodo);
